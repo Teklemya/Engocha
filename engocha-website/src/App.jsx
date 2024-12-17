a842821554a9aa8701400f2a771891dadefb372b
Teklemya <Teklemya@mail.uc.edu>
2024-12-16 18:12:50 -0600

    added menu to teh app

--- engocha-website/src/App.jsx
+++ engocha-website/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Hero from './Components/Hero/Hero';
-// import Menu from './Menu';
+import Home from './Components/Home/Home'
+import Navbar from './Components/Navbar/Navbar';
+import Menu from './Components/Menu/Menu';
 // import About from './About';
 // import Contact from './Contact';
 // import Catering from './Catering';
@@ -9,10 +10,11 @@ import './App.css';
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Hero />} />
-        {/* <Route path="/menu" element={<Menu />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+         <Route path="/menu" element={<Menu />} />
+        {/*<Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/catering" element={<Catering />} /> */}
       </Routes>
